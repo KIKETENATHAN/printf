9f609d32f36c22eb5e7b408fe511fb10580a5aac
KIKETENATHAN <kiketenathan@gmail.com>
2023-08-20 11:10:40 -0700

    task 0 corrections

--- _printf.c
+++ _printf.c
@@ -11,14 +11,47 @@ int _printf(const char *format, ...)
 	int count = 0;
 	va_list args;
 
+	va_start(args, format);
+
 	if (format == NULL)
 		return (-1);
 	va_start(args, format);
-	while (*format)
+	while (*format != '\0')
 	{
 		if (*format != '%')
 		{
 			write(1, format, 1);
+			count++;
+		}
+		else
+		{
+			format++;
+			if (*format == 'c')
+			{
+				char c = va_arg(args, int);
+
+				write(1, &c, 1);
+				count++;
+			}
+			else if (*format == 's')
+			{
+				char *str = va_arg(args, char *);
+
+				if (str)
+				{
+					while (*str != '\0')
+					{
+						write(1, str, 1);
+						str++;
+						count++;
+					}
+				}
+			}
+			else if (*format == '%')
+			{
+				write(1, "%", 1);
+				count++;
+			}
 		}
 		format++;
 	}
